1feab725cfdf4b6b0c53126f79a0180217d3371d
pawelqus <pawelqus@gmail.com>
2019-07-31 09:56:54 +0200

    Add rounded bars

--- ChartsDemo-iOS/Swift/Demos/BarChartViewController.swift
+++ ChartsDemo-iOS/Swift/Demos/BarChartViewController.swift
@@ -42,57 +42,67 @@ class BarChartViewController: DemoBaseViewController {
         
         chartView.maxVisibleCount = 60
         
-        let xAxis = chartView.xAxis
-        xAxis.labelPosition = .bottom
-        xAxis.labelFont = .systemFont(ofSize: 10)
-        xAxis.granularity = 1
-        xAxis.labelCount = 7
-        xAxis.valueFormatter = DayAxisValueFormatter(chart: chartView)
         
-        let leftAxisFormatter = NumberFormatter()
-        leftAxisFormatter.minimumFractionDigits = 0
-        leftAxisFormatter.maximumFractionDigits = 1
-        leftAxisFormatter.negativeSuffix = " $"
-        leftAxisFormatter.positiveSuffix = " $"
+        self.chartView.xAxis.drawGridLinesEnabled = false
+        self.chartView.xAxis.drawLabelsEnabled = false
+        self.chartView.leftAxis.drawGridLinesEnabled = false
+        self.chartView.leftAxis.drawLabelsEnabled = false
+        self.chartView.legend.enabled = false
+        self.chartView.borderColor = UIColor.clear
+        self.chartView.xAxis.drawAxisLineEnabled = false
+        self.chartView.leftAxis.drawAxisLineEnabled = false
         
-        let leftAxis = chartView.leftAxis
-        leftAxis.labelFont = .systemFont(ofSize: 10)
-        leftAxis.labelCount = 8
-        leftAxis.valueFormatter = DefaultAxisValueFormatter(formatter: leftAxisFormatter)
-        leftAxis.labelPosition = .outsideChart
-        leftAxis.spaceTop = 0.15
-        leftAxis.axisMinimum = 0 // FIXME: HUH?? this replaces startAtZero = YES
-        
-        let rightAxis = chartView.rightAxis
-        rightAxis.enabled = true
-        rightAxis.labelFont = .systemFont(ofSize: 10)
-        rightAxis.labelCount = 8
-        rightAxis.valueFormatter = leftAxis.valueFormatter
-        rightAxis.spaceTop = 0.15
-        rightAxis.axisMinimum = 0
-        
-        let l = chartView.legend
-        l.horizontalAlignment = .left
-        l.verticalAlignment = .bottom
-        l.orientation = .horizontal
-        l.drawInside = false
-        l.form = .circle
-        l.formSize = 9
-        l.font = UIFont(name: "HelveticaNeue-Light", size: 11)!
-        l.xEntrySpace = 4
+//        let xAxis = chartView.xAxis
+//        xAxis.labelPosition = .bottom
+//        xAxis.labelFont = .systemFont(ofSize: 10)
+//        xAxis.granularity = 1
+//        xAxis.labelCount = 7
+//        xAxis.valueFormatter = DayAxisValueFormatter(chart: chartView)
+//
+//        let leftAxisFormatter = NumberFormatter()
+//        leftAxisFormatter.minimumFractionDigits = 0
+//        leftAxisFormatter.maximumFractionDigits = 1
+//        leftAxisFormatter.negativeSuffix = " $"
+//        leftAxisFormatter.positiveSuffix = " $"
+//
+//        let leftAxis = chartView.leftAxis
+//        leftAxis.labelFont = .systemFont(ofSize: 10)
+//        leftAxis.labelCount = 8
+//        leftAxis.valueFormatter = DefaultAxisValueFormatter(formatter: leftAxisFormatter)
+//        leftAxis.labelPosition = .outsideChart
+//        leftAxis.spaceTop = 0.15
+//        leftAxis.axisMinimum = 0 // FIXME: HUH?? this replaces startAtZero = YES
+//
+//        let rightAxis = chartView.rightAxis
+//        rightAxis.enabled = true
+//        rightAxis.labelFont = .systemFont(ofSize: 10)
+//        rightAxis.labelCount = 8
+//        rightAxis.valueFormatter = leftAxis.valueFormatter
+//        rightAxis.spaceTop = 0.15
+//        rightAxis.axisMinimum = 0
+//
+//        let l = chartView.legend
+//        l.horizontalAlignment = .left
+//        l.verticalAlignment = .bottom
+//        l.orientation = .horizontal
+//        l.drawInside = false
+//        l.form = .circle
+//        l.formSize = 9
+//        l.font = UIFont(name: "HelveticaNeue-Light", size: 11)!
+//        l.xEntrySpace = 4
 //        chartView.legend = l
 
-        let marker = XYMarkerView(color: UIColor(white: 180/250, alpha: 1),
-                                  font: .systemFont(ofSize: 12),
-                                  textColor: .white,
-                                  insets: UIEdgeInsets(top: 8, left: 8, bottom: 20, right: 8),
-                                  xAxisValueFormatter: chartView.xAxis.valueFormatter!)
-        marker.chartView = chartView
-        marker.minimumSize = CGSize(width: 80, height: 40)
-        chartView.marker = marker
+//        let marker = XYMarkerView(color: UIColor(white: 180/250, alpha: 1),
+//                                  font: .systemFont(ofSize: 12),
+//                                  textColor: .white,
+//                                  insets: UIEdgeInsets(top: 8, left: 8, bottom: 20, right: 8),
+//                                  xAxisValueFormatter: chartView.xAxis.valueFormatter!)
+//        marker.chartView = chartView
+//        marker.minimumSize = CGSize(width: 80, height: 40)
+//        chartView.marker = marker
         
-        sliderX.value = 12
-        sliderY.value = 50
+//        sliderX.value = 12
+//        sliderY.value = 50
         slidersValueChanged(nil)
     }
     
@@ -110,12 +120,8 @@ class BarChartViewController: DemoBaseViewController {
         
         let yVals = (start..<start+count+1).map { (i) -> BarChartDataEntry in
             let mult = range + 1
-            let val = Double(arc4random_uniform(mult))
-            if arc4random_uniform(100) < 25 {
-                return BarChartDataEntry(x: Double(i), y: val, icon: UIImage(named: "icon"))
-            } else {
-                return BarChartDataEntry(x: Double(i), y: val)
-            }
+            let val = Double(arc4random_uniform(mult) + 1)
+            return BarChartDataEntry(x: Double(i), y: val)
         }
         
         var set1: BarChartDataSet! = nil
